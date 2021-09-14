2e103d03a573f38a23d5c5643d73380af48d2983
Teayeon-Lim <wingofyeon@naver.com>
2021-09-15 00:08:54 +0900

    feature/add button styling

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 
 - extension: vscode-styled-components
 
-### Use simple styled-component
+### Use styled-component
 
-styled-components 첫 적용: 단순 원 스타일링
-css를 적용, 내부 'template literals' => 'tagged template literals'로 사용
+- styled-components 첫 적용: 단순 원 스타일링
+- {css} 적용, 내부 'template literals' => 'tagged template literals'로 사용
+- Button styling
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import './App.css';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
+import Button from './Components/Button';
 
 // styled-components
-const Circle = styled.div`
-  width: 5rem;
-  height: 5rem;
-  background: ${props => props.color};
-  border-radius: 50%;
-  ${props =>
-    props.huge &&
-    // template literals('css' 미적용) => tagged template literals (적용)
-    css`
-      width: 10rem;
-      height: 10rem;
-    `}
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
 `;
 
 function App() {
-  return <Circle color='blue' huge />;
+  return (
+    <AppBlock>
+      <Button>BUTTON</Button>
+    </AppBlock>
+  );
 }
 
 export default App;
