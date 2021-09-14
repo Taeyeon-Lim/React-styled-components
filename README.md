38d9a838276bb5cb3051b139bf214215194e9bbb
Teayeon-Lim <wingofyeon@naver.com>
2021-09-14 23:42:04 +0900

    feature/styled-component 적용

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # styled-component init
 
 ### Add styled-component lib
+
+- extension: vscode-styled-components
+
+### Use simple styled-component
+
+styled-components 첫 적용: 단순 원 스타일링
+css를 적용, 내부 'template literals' => 'tagged template literals'로 사용
--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import styled, { css } from 'styled-components';
+
+// styled-components
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${props => props.color};
+  border-radius: 50%;
+  ${props =>
+    props.huge &&
+    // template literals('css' 미적용) => tagged template literals (적용)
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Circle color='blue' huge />;
 }
 
 export default App;
